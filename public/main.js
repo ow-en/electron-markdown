7c1f12dc844a40dfc2bbb1ac9e78a71f9d2c18f8
owensource <ciruz001@cougars.csusm.edu>
2018-04-08 17:19:02 -0700

    Deployment build successful

--- public/main.js
+++ public/main.js
@@ -1,32 +1,34 @@
-const electron = require('electron');
-const app = electron.app;
+const {app} = require('electron');
 const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+const isDev = require('electron-is-dev');
 
 let mainWindow;
 
 function createWindow() {
     mainWindow = new BrowserWindow({width: 900, height: 680});
-    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
 
     app.setAboutPanelOptions({
-        applicationName: "Mook",
-        applicationVersion: "0.0.1",
+      applicationName: “electronmd”,
+      applicationVersion: “0.0.1”,
     })
 
     mainWindow.on('closed', () => mainWindow = null);
-}
+  }
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        app.quit();
+      app.quit();
     }
-});
-
-app.on('activate', () => {
+  });
+  
+  app.on('activate', () => {
     if (mainWindow === null) {
-        createWindow();
+      createWindow();
     }
-});
-
+  });
