80ff1a7fdaf9a27662d28ab4eb86306e886048e9
owensource <ciruz001@cougars.csusm.edu>
2018-04-16 20:20:00 -0700

    added basic md functionality

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SplitPane from 'react-split-pane';
+import Editor from './editor.js';
 
 class App extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      markdownSrc: "# Hello World",
+    }
+  }
   render() {
     return (
       <div className="App">
         <SplitPane split="vertical" defaultSize="50%">
           <div className="editor-pane">
+            <Editor className="editor" value=
+            {this.state.markdownSrc}/>
           </div>
           <div className="view-pane">
           </div>
         </SplitPane>
       </div>
-    )
-  };
+    );
+  }
 }
 
 export default App;
